02174c92a91eb973c28b4fc3157fa06f0950824d
ecc97 <carmonaedwin1123@gmail.com>
2024-08-05 13:00:01 -0500

    feat: created post functions

--- src/Controllers/Posts.controller.ts
+++ src/Controllers/Posts.controller.ts
@@ -13,4 +13,21 @@ export class PostsController {
         console.log(response.status)
         return data
     }
+
+    async createPost(endPoint: string, dataPost: IPost): Promise<IPost> {
+        // Hace una petición POST a la API para agregar un Post
+        const response = await fetch(`${this.url}${endPoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' // Define el tipo de contenido como JSON
+            },
+            body: JSON.stringify(dataPost) // Convierte los datos de la ciudad a una cadena JSON para enviarlos en el cuerpo de la solicitud
+        });
+        const data: IPost = await response.json(); // Convierte la respuesta de la API a un objeto JSON
+        if (response.status !== 201) { // Verifica si el estado de la respuesta no es 201 (Created)
+            throw new Error('No se pudo agregar la ciudad'); // Lanza un error si el estado de la respuesta no es 201
+        }
+        console.log(response.status); // Imprime el estado de la respuesta en la consola
+        return data; // Devuelve los datos de la ciudad agregada
+    }
 }
